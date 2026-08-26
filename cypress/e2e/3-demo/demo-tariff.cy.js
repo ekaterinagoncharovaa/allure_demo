d8935a788c9b99756b84133ca49181018a2df2fd
ekaterinagoncharovaa <ekaterina.goncharova@qameta.io>
2026-08-26 16:31:06 +0200

    Update demo-tariff.cy.js

--- cypress/e2e/3-demo/demo-tariff.cy.js
+++ cypress/e2e/3-demo/demo-tariff.cy.js
@@ -1,11 +1,16 @@
 import * as allure from "allure-js-commons";
-const { it } = require("mocha");
-const allure = require("allure-js-commons");
-
-it("Subscriber can switch to a different tariff plan @allure.id:75553", async() => {
-    await allure.owner("kategonc");
-    await allure.step("Open the tariff plans page", async () => {});
-    await allure.step("Select the Unlimited Plus plan", async () => {});
-    await allure.step("Confirm the switch", async () => {});
-    await allure.step("Verify the new plan is shown as active", async () => {});
+it("Subscriber can switch to a different tariff plan @allure.id:75553", () => {
+  allure.owner("kategonc");
+  allure.step("Open the tariff plans page", () => {
+    expect(true).to.eq(true);
+  });
+  allure.step("Select the Unlimited Plus plan", () => {
+    expect(true).to.eq(true);
+  });
+  allure.step("Confirm the switch", () => {
+    expect(true).to.eq(true);
+  });
+  allure.step("Verify the new plan is shown as active", () => {
+    expect(true).to.eq(true);
+  });
 });
